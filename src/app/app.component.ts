7f6d92e98ec889cc1dd169132aad49e1948aa7c0
tariqrubaia <>
2021-10-29 06:19:50 +0300

    my test approval 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test good morning 1111';
+  title = 'angular-test good morning 2222';
 }
